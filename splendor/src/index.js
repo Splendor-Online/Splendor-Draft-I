04dc5165385a0f7b2f4926e4141ab552d5fe3ff8
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-24 11:33:26 +0900

    Update import path to apply the renamed file (previous commit)

--- splendor/src/index.js
+++ splendor/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/index';
+import App from './components/App/index.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
