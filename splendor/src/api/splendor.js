e969a3d9bdb360abf1d7ecb996b8a4820a8c1436
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-26 16:38:11 +0900

    Fix outdated user_data search api

--- splendor/src/api/splendor.js
+++ splendor/src/api/splendor.js
@@ -5,6 +5,14 @@ function get_user_data(user_name) {
   // Bring user data from server.
   return fetch(`${HOST}/user/${user_name}`, { method: 'GET' })
     .then(res => res.json())
+    .then(res => {
+      if (res.error) {
+        throw Error("Could not get userdata from server.");
+      }
+      else {
+        return res.data
+      }
+    })
 }
 
 
